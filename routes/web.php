da78d6af732715ddc5e278da34b3a28e166b3d50
DeteSher <357791771@qq.com>
2019-04-01 07:28:51 +0000

    中文的错误提示信息

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
-Route::get('users/{user}', 'UsersController@show')->name('users.show');
 /*
 Route::resource('users', 'UsersController');
 等同于以下代码
