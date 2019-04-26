edcd9294de39e57aa3b95a7ee5de12ba79e4f2f8
DeteSher <357791771@qq.com>
2019-04-26 07:30:23 +0000

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,7 +23,7 @@ class UsersController extends Controller
     public function index()
     {
         $users = User::paginate(10);
-        return view('users.index', compact('users'));
+        return view('users.index', compact('users', 'statuses'));
     }
 
     public function create ()
@@ -33,7 +33,8 @@ class UsersController extends Controller
 
     public function show (User $user)
     {
-    	return view('users.show', compact('user'));
+        $statuses = $user->statuses()->orderBy('created_at', 'desc')->paginate(10);
+    	return view('users.show', compact('user', 'statuses'));
     }
 
     public function edit(User $user)
